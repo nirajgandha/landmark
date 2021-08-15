f7e5e31a7be87e9d6e20012d19d73a8003fa9c15
Niraj Gandha <nirajgandha23@gmail.com>
2021-08-15 19:32:24 +0530

    v0.0
    No changes done. Just initial commit to create different branches.
    
    Signed-off-by: Niraj Gandha <nirajgandha23@gmail.com>

---
+++ app/src/main/java/com/shreenology/landmark/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.shreenology.landmark
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
